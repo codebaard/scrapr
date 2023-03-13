b5a1a4f6802a8aca55705d57ff02dda4076f623d
codebaard <github@juliusneudecker.com>
2023-03-13 21:41:47 +0100

    added initialized springboot project

---
+++ src/main/java/com/scrapr/ScraprApplication.java
@@ -0,0 +1,13 @@
+package com.scrapr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScraprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScraprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrapr/ScraprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrapr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScraprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
